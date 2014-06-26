2466b8d695a87beba36713447521248478c02940
mmterpstra <m.m.terpstra@umcg.nl>
2014-06-26 17:34:57 +0200

    fix htseq-count time and parameterised Generates scripts

--- GenerateScripts.sh
+++ GenerateScripts.sh
@@ -1,29 +1,33 @@
 #!bin/bash
 
+#main thing to remember when working with molgenis "/full/paths" ALWAYS!
+#here some parameters for customisation
 tmp="tmp01"
+runDir=/gcc/groups/oncogenetics/${tmp}/projects/resourcesRNASeq/Workflow/runs/testlong
+molgenisBase=/gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT
 
 echo "copy resources to tmp"
-cp -r /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/resources /gcc/groups/oncogenetics/$tmp/
-cp -r /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/tools /gcc/groups/oncogenetics/$tmp/
+cp -r $molgenisBase/workflows/RnaAnalysis/resources /gcc/groups/oncogenetics/$tmp/
+cp -r $molgenisBase/workflows/RnaAnalysis/tools /gcc/groups/oncogenetics/$tmp/
 echo "Convert parametersheet"
-perl /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/scripts/convertParametersGitToMolgenis.pl parameters.csv > parameters.molgenis.csv
+perl $molgenisBase/workflows/RnaAnalysis/scripts/convertParametersGitToMolgenis.pl parameters.csv > parameters.molgenis.csv
 
 echo "Generate scripts"
 module load jdk/1.7.0_51
 
 #runID=++
-runDir=/gcc/groups/oncogenetics/${tmp}/projects/resourcesRNASeq/Workflow/runs/testlong
+
 rm $runDir.*d
 
-bash /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/molgenis_compute.sh \
+bash $molgenisBase/molgenis_compute.sh \
  --generate \
- -p /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/parameters.molgenis.csv \
- -p /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/SamplesheetTestLong.csv \
- -w /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/workflows/RnaAnalysis/workflow.csv \
+ -p $(pwd)/parameters.molgenis.csv \
+ -p $molgenisBase/workflows/RnaAnalysis/SamplesheetTestLong.csv \
+ -w $(pwd)/workflow.csv \
  --backend pbs \
  --weave \
  -rundir $runDir \
  -runid 02 \
- -header /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/templates/pbs/header.ftl \
- -submit /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/templates/pbs/submit.ftl \
- -footer /gcc/groups/oncogenetics/prm02/data/mytools/molgenis-compute-core-1.0.0-SNAPSHOT/templates/pbs/footer.ftl 
+ -header $molgenisBase/templates/pbs/header.ftl \
+ -submit $molgenisBase/templates/pbs/submit.ftl \
+ -footer $molgenisBase/templates/pbs/footer.ftl 
